70c5823edce25950e1df86d05c18dc82d0032aca
Harshith <87645227+HarshithPaladi@users.noreply.github.com>
2023-03-12 22:25:26 +0530

    Integration of effects with basic text

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,56 @@ Array.from(document.getElementsByClassName("menu-item"))
       menu.dataset.activeIndex = index;
     }
   });
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => {
+  const { clientX, clientY } = event;
+
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
+/* -- Text effect -- */
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+Array.from(document.getElementsByTagName("a"))
+  .forEach((link) => {
+    link.onmouseover = (event) => {
+      let iteration = 0;
+
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return event.target.dataset.value[index];
+            }
+
+            return letters[Math.floor(Math.random() * 26)]
+          })
+          .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 40);
+    };
+    link.onmouseout = () => {
+      clearInterval(interval);
+
+      interval = null;
+
+      link.innerText = link.dataset.value;
+    };
+  });
